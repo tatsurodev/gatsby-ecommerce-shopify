707f69fd75079d2909b11c598837ae5c00b2f966
Tatsuro <contacts@tatsuro.dev>
2021-12-02 21:23:48 +0900

    product templateでpage query作成 13

--- gatsby-shopify-course/src/templates/ProductTemplate/index.js
+++ gatsby-shopify-course/src/templates/ProductTemplate/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+
+// page queryでは、graphql内でpageContextの変数にaccessできる
+export const query = graphql`
+  query ProductQuery($shopifyId: String) {
+    shopifyProduct(shopifyId: { eq: $shopifyId }) {
+      title
+    }
+  }
+`;
 
 export default function ProductTemplate(props) {
   console.log(props);
-  return <h1>Product</h1>;
+  return <h1>{props.data.shopifyProduct.title}</h1>;
 }
