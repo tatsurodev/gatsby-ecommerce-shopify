3814348735782528d529ba94e45441087f524835
Tatsuro <contacts@tatsuro.dev>
2021-12-03 20:40:25 +0900

    variant idをurlに使用 22

--- gatsby-shopify-course/src/templates/ProductTemplate/index.js
+++ gatsby-shopify-course/src/templates/ProductTemplate/index.js
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/no-onchange */
 import React from 'react';
 import { graphql } from 'gatsby';
 import { Layout, ImageGallery } from 'components';
 import { Grid, SelectWrapper, Price } from './styles';
 import CartContext from 'context/CartContext';
+import { navigate, useLocation } from '@reach/router';
 
 // page queryでは、graphql内でpageContextの変数にaccessできる
 export const query = graphql`
@@ -29,16 +31,27 @@ export default function ProductTemplate(props) {
   const { getProductById } = React.useContext(CartContext);
   const [product, setProduct] = React.useState(null);
   const [selectedVariant, setSelectedVariant] = React.useState(null);
+  const { search, origin, pathname } = useLocation();
+  // search: search query, origin: main domain name, pathname: url - origin - search
+  console.log(search, origin, pathname);
 
   React.useEffect(() => {
     getProductById(props.data.shopifyProduct.shopifyId).then(result => {
       setProduct(result);
       setSelectedVariant(result.variants[0]);
     });
-  }, [getProductById, setProduct]);
+  }, [getProductById, setProduct, props.data.shopifyProduct.shopifyId]);
 
   const handleVariantChange = e => {
-    setSelectedVariant(product?.variants.find(v => v.id === e.target.value));
+    const newVariant = product?.variants.find(v => v.id === e.target.value);
+    setSelectedVariant(newVariant);
+    navigate(
+      // variant idの中にURIに使用できない特殊文字(漢字、ひらがな、<>など)が含まれる可能性があるので、encodeURIComponentでencode
+      `${origin}${pathname}?variant=${encodeURIComponent(newVariant.id)}`,
+      {
+        replace: true,
+      }
+    );
   };
 
   return (
