116f32f7b75a7802558c9e867ea3c885dc24481e
Tatsuro <contacts@tatsuro.dev>
2021-12-05 05:12:50 +0900

    ProductQuantityAdderにqty state追加 28

--- gatsby-shopify-course/src/components/ProductQuantityAdder/index.js
+++ gatsby-shopify-course/src/components/ProductQuantityAdder/index.js
@@ -4,12 +4,33 @@ import { Input } from '../Input';
 import { ProductQuantityAdderWrapper } from './styles';
 
 export function ProductQuantityAdder({ variantId, available }) {
+  const [quantity, setQuantity] = React.useState(1);
+
+  // e.target はイベントを発生させる原因となった要素
+  // e.currentTarget はイベントハンドラが実際に付与された要素
+  const handleQuantityChange = e => {
+    setQuantity(e.currentTarget.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
     <ProductQuantityAdderWrapper>
       <strong>Quantity</strong>
-      <form>
-        <Input />
-        <Button fullWidth>Add to card</Button>
+      <form onSubmit={handleSubmit}>
+        <Input
+          disabled={!available}
+          type="number"
+          min="1"
+          step="1"
+          value={quantity}
+          onChange={handleQuantityChange}
+        />
+        <Button type="submit" disabled={!available} fullWidth>
+          Add to card
+        </Button>
       </form>
     </ProductQuantityAdderWrapper>
   );
