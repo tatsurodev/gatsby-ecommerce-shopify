b189e270a2c6f30f37e8a0f440fe96676e9ed477
Tatsuro <contacts@tatsuro.dev>
2021-12-05 20:56:30 +0900

    CartContentsのstyling 34

--- gatsby-shopify-course/src/components/CartContents/index.js
+++ gatsby-shopify-course/src/components/CartContents/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CartContext from 'context/CartContext';
-import { CartItem } from './styles';
+import { CartItem, CartHeader, CartFooter } from './styles';
 
 export function CartContents() {
   const { checkout } = React.useContext(CartContext);
@@ -8,17 +8,33 @@ export function CartContents() {
   return (
     <section>
       <h1>Your cart</h1>
+      <CartHeader>
+        <div>Product</div>
+        <div>Unit price</div>
+        <div>Quantity</div>
+        <div>Amount</div>
+      </CartHeader>
       {checkout?.lineItems?.map(item => (
         <CartItem key={item.variant.id}>
           <div>
             <div>{item.title}</div>
-            <div>{item.variant.title}</div>
+            <div>
+              {item.variant.title === 'Default Title' ? '' : item.variant.title}
+            </div>
           </div>
           <div>£{item.variant.price}</div>
           <div>{item.quantity}</div>
-          <div>{(item.quantity * item.variant.price).toFixed(2)}</div>
+          <div>£{(item.quantity * item.variant.price).toFixed(2)}</div>
         </CartItem>
       ))}
+      <CartFooter>
+        <div>
+          <strong>total:</strong>
+        </div>
+        <div>
+          <span>£{checkout?.totalPrice}</span>
+        </div>
+      </CartFooter>
     </section>
   );
 }
