c9eba242524f9f5886dc29e0909d0bad30674e66
Tatsuro <contacts@tatsuro.dev>
2021-12-12 05:42:16 +0900

    sale tileのstyling 41

--- gatsby-shopify-course/src/components/HomepageCollectionsGrid/index.js
+++ gatsby-shopify-course/src/components/HomepageCollectionsGrid/index.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { CollectionTile } from '../CollectionTile';
+import { RemainingCollections } from './styles';
 
 export function HomepageCollectionsGrid({ collections }) {
+  const saleCollection = collections.find(
+    collection => collection.title === 'SALE'
+  );
+  const remainingCollections = collections.filter(
+    collection => collection.title !== 'SALE'
+  );
+
   return (
     <div>
-      {collections.map(collection => (
+      {!!saleCollection && (
         <CollectionTile
-          title={collection.title}
-          description={collection.description}
-          // imageがnullなcollectionもあるのでoptional
-          backgroundImage={collection.image?.localFile.childImageSharp.fluid}
-          key={collection.shopifyId}
+          sale
+          title={saleCollection.title}
+          description={saleCollection.description}
+          backgroundImage={
+            saleCollection.image?.localFile.childImageSharp.fluid
+          }
         />
-      ))}
+      )}
+      <RemainingCollections>
+        {remainingCollections.map(collection => (
+          <CollectionTile
+            title={collection.title}
+            description={collection.description}
+            // imageがnullなcollectionもあるのでoptional
+            backgroundImage={collection.image?.localFile.childImageSharp.fluid}
+            key={collection.shopifyId}
+          />
+        ))}
+      </RemainingCollections>
     </div>
   );
 }
