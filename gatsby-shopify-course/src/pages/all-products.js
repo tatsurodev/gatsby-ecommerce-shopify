44da35b79a7abf48a6db425b5171a7b6bd7b9b15
Tatsuro <contacts@tatsuro.dev>
2021-12-13 08:44:29 +0900

    Filters, Checkbox component作成 46

--- gatsby-shopify-course/src/pages/all-products.js
+++ gatsby-shopify-course/src/pages/all-products.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Layout } from 'components';
+import { Layout, Filters } from 'components';
 import Productcontext from 'context/ProductContext';
+import styled from 'styled-components';
+
+// src/pages directoryにstyles.jsを配置してしまうとすべてpageとして処理されてしまうので同じpage内のcodeでstyle系のwrapperを作成する
+const Content = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  margin-top: 20px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 export default function AllProducts() {
   const { products, collections } = React.useContext(Productcontext);
@@ -9,11 +18,10 @@ export default function AllProducts() {
   return (
     <Layout>
       <h4>{products.length} products</h4>
-      <div>
-        {collections.map(collection => (
-          <div key={collection.shopifyId}>{collection.title}</div>
-        ))}
-      </div>
+      <Content>
+        <Filters />
+        <div>Products</div>
+      </Content>
     </Layout>
   );
 }
