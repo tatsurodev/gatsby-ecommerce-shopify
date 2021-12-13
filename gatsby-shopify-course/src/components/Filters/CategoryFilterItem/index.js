5e105f0c1a38b69ec5a2a03052981c7903291c1e
Tatsuro <contacts@tatsuro.dev>
2021-12-13 10:04:58 +0900

    categoryをclickした時にurl queryにcategory idを追加してrouting 47

--- gatsby-shopify-course/src/components/Filters/CategoryFilterItem/index.js
+++ gatsby-shopify-course/src/components/Filters/CategoryFilterItem/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Checkbox } from 'components';
+import { navigate, useLocation } from '@reach/router';
 import { CategoryFilterItemWrapper } from './styles';
+import queryString from 'query-string';
 
-export function CategoryFilterItem({ title }) {
+export function CategoryFilterItem({ title, id }) {
+  const { search } = useLocation();
+  // queryをparseして、keyとvalueで分けてobjectに
+  const qs = queryString.parse(search);
+  const collectionId = qs.c;
+
+  const onClick = () => {
+    let navigateTo = '/all-products';
+    navigate(`${navigateTo}?c=${encodeURIComponent(id)}`);
+  };
+
   return (
-    <CategoryFilterItemWrapper>
-      <Checkbox />
+    <CategoryFilterItemWrapper onClick={onClick}>
+      <Checkbox checked={collectionId === id} />
       <div>{title}</div>
     </CategoryFilterItemWrapper>
   );
