c06bdedeb7f144c33242c2b1a3b33ae8c2143f7e
Tatsuro <contacts@tatsuro.dev>
2021-12-14 16:29:50 +0900

    categoryのcheck, uncheckでurlを生成、routing 48

--- gatsby-shopify-course/src/components/Filters/CategoryFilterItem/index.js
+++ gatsby-shopify-course/src/components/Filters/CategoryFilterItem/index.js
@@ -8,16 +8,34 @@ export function CategoryFilterItem({ title, id }) {
   const { search } = useLocation();
   // queryをparseして、keyとvalueで分けてobjectに
   const qs = queryString.parse(search);
-  const collectionId = qs.c;
+  // ,の後にemptyな要素があるかもしれないのでそれを排除、query自体がなければcollection idを格納する配列を初期化
+  const collectionIds = qs.c?.split(',').filter(c => !!c) || [];
+  const checked = collectionIds?.find(cId => cId === id);
 
   const onClick = () => {
     let navigateTo = '/all-products';
-    navigate(`${navigateTo}?c=${encodeURIComponent(id)}`);
+    let newIds = [];
+    // checkedがtrueの場合、urlからcolllection idを取り除く
+    if (checked) {
+      newIds = collectionIds
+        .filter(cId => cId !== id)
+        .map(cId => encodeURIComponent(cId));
+      // checkedがfalseの場合、urlにcollection idを追加
+    } else {
+      collectionIds.push(id);
+      newIds = collectionIds.map(cId => encodeURIComponent(cId));
+    }
+    // categoryが選択されていない、つまりcollection idが0の時、?cのqueryをつけないようにする
+    if (newIds.length) {
+      navigate(`${navigateTo}?c=${newIds.join(',')}`);
+    } else {
+      navigate(`${navigateTo}`);
+    }
   };
 
   return (
     <CategoryFilterItemWrapper onClick={onClick}>
-      <Checkbox checked={collectionId === id} />
+      <Checkbox checked={checked} />
       <div>{title}</div>
     </CategoryFilterItemWrapper>
   );
